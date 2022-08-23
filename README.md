c339a71111cb413ce86ac222c96a73dca90898c1
Jette <dohnjette.deguzman@gmail.com>
2022-08-24 00:54:37 +0800

    Changed the color of the pen and made input and btn work

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # etch-a-sketch
+
+you can try the live page here https://djbdeguzman.github.io/etch-a-sketch/
+
+as of August 24,2022 I was able to make the color button change the color, however,
+If I select color blue for example then go back to color black, it won't go back to previous color (black).
+
+I decided to remove the change color button for now and used a pastel RGB instead as it looks better for me.
+
+I will make it look better soon!
--- index.js
+++ index.js
@@ -13,6 +13,39 @@ function makeDivGrid(col,rows) {
     }
 }
 
+//this will limit the input to 3 digits and will not allow non number entry
+document.getElementById('numberOnly').addEventListener('keypress', event => {
+    if (!`${event.target.value}${event.key}`.match(/^[0-9]{0,3}$/)) {
+        // block the input if result does not match
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
+    }
+});
+
+
+//function onClickSubmitValue will get the input value
+function onClickSubmitValue() {
+    const userInputGridSize = document.querySelector(".sizeInput").value;
+    if(userInputGridSize < 2 || userInputGridSize>100){
+        showError(); // call showError function
+    } else {
+        noError(); // call noError function which removes the error message
+        return userInputGridSize;
+    }
+}
+
+//show error message if input is not 2 to 100
+function showError() { 
+    let errorMessage = document.getElementById("error");
+    errorMessage.style.display = "block";
+  }
+//removes the error message if the input number is valid
+function noError() { 
+    let errorMessage = document.getElementById("error");
+    errorMessage.style.display = "none";
+  }
+
 //color pen
 
 boxDivsAddEventBlack(); //run default pen black
@@ -20,10 +53,37 @@ boxDivsAddEventBlack(); //run default pen black
 function boxDivsAddEventBlack() {
   let allTiles = document.querySelectorAll(".box");
   allTiles.forEach(element => {
-    element.addEventListener("mouseover", black);
+    element.addEventListener("mouseover", pastelRGB);
   });
 }
+
+function pastelRGB(event) {
+    let a = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let c = Math.floor(Math.random() * 256);
+    event.target.style.backgroundColor = `rgb(${a}, ${b}, ${c})`;
+    event.target.style.opacity += 0.3;
+  }
+  
 
 function black() {
   this.classList.add("hoveredBlack");
 }//this function changes the class of the box to hovered black
+
+//function to remove the highlighted box
+function removeAllBoxDivs() {
+    
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
+
+// Change Grid Size button and reset button
+function submitButtonEvent() { 
+    let num = onClickSubmitValue();
+    onClickSubmitValue();
+    removeAllBoxDivs();
+    makeDivGrid(num,num);
+    boxDivsAddEventBlack();
+}
+
